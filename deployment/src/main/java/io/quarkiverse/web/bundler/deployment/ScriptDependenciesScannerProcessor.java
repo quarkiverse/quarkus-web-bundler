b10e46d95d9da2f092a350db3b5ea5d369656297
Phillip Kruger <phillip.kruger@gmail.com>
2023-09-25 17:50:12 +1000

    Allow provided dependencies
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- deployment/src/main/java/io/quarkiverse/web/bundler/deployment/ScriptDependenciesScannerProcessor.java
+++ deployment/src/main/java/io/quarkiverse/web/bundler/deployment/ScriptDependenciesScannerProcessor.java
@@ -2,16 +2,20 @@ package io.quarkiverse.web.bundler.deployment;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.jboss.logging.Logger;
 
 import io.quarkiverse.web.bundler.deployment.items.WebDependenciesBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.ApplicationArchivesBuildItem;
 import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
 import io.quarkus.maven.dependency.Dependency;
+import io.quarkus.maven.dependency.DependencyFlags;
 import io.quarkus.maven.dependency.ResolvedDependency;
 import io.quarkus.paths.PathCollection;
 
@@ -20,17 +24,30 @@ class ScriptDependenciesScannerProcessor {
     private static final Logger LOGGER = Logger.getLogger(ScriptDependenciesScannerProcessor.class);
 
     @BuildStep
-    WebDependenciesBuildItem collectDependencies(ApplicationArchivesBuildItem applicationArchives,
-            CurateOutcomeBuildItem curateOutcome,
+    WebDependenciesBuildItem collectDependencies(CurateOutcomeBuildItem curateOutcome,
             WebBundlerConfig config)
             throws IOException {
-        List<Path> webDeps = curateOutcome.getApplicationModel().getRuntimeDependencies().stream()
-                .filter(Dependency::isJar)
+
+        Set<Path> webDeps = new HashSet<>();
+
+        Iterable<ResolvedDependency> runDependencies = curateOutcome.getApplicationModel()
+                .getDependencies(DependencyFlags.RUNTIME_CP);
+        webDeps.addAll(getWebDependencies(runDependencies, config));
+
+        Iterable<ResolvedDependency> compileDependencies = curateOutcome.getApplicationModel()
+                .getDependencies(DependencyFlags.COMPILE_ONLY);
+        webDeps.addAll(getWebDependencies(compileDependencies, config));
+
+        return new WebDependenciesBuildItem(config.dependencies().type(), List.copyOf(webDeps));
+    }
+
+    private Set<Path> getWebDependencies(Iterable<ResolvedDependency> dependencies, WebBundlerConfig config) {
+        Stream<ResolvedDependency> dependenciesStream = StreamSupport.stream(dependencies.spliterator(), false);
+        return dependenciesStream.filter(Dependency::isJar)
                 .filter(config.dependencies().type()::matches)
                 .map(ResolvedDependency::getResolvedPaths)
                 .flatMap(PathCollection::stream)
                 .filter(p -> p.getFileName().toString().endsWith(".jar"))
-                .collect(Collectors.toList());
-        return new WebDependenciesBuildItem(config.dependencies().type(), webDeps);
+                .collect(Collectors.toSet());
     }
 }
