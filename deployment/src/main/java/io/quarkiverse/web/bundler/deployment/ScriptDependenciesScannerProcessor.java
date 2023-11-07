ca432742b86668cbafa494eb0d82492b84a4b820
Andy Damevin <ia3andy@gmail.com>
2023-11-07 11:46:00 +0100

    Exclude mvnpm and webjars jars from target build

--- deployment/src/main/java/io/quarkiverse/web/bundler/deployment/ScriptDependenciesScannerProcessor.java
+++ deployment/src/main/java/io/quarkiverse/web/bundler/deployment/ScriptDependenciesScannerProcessor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.jboss.logging.Logger;
 
@@ -12,6 +13,7 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.ApplicationArchivesBuildItem;
 import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
 import io.quarkus.maven.dependency.Dependency;
+import io.quarkus.maven.dependency.DependencyFlags;
 import io.quarkus.maven.dependency.ResolvedDependency;
 import io.quarkus.paths.PathCollection;
 
@@ -24,7 +26,8 @@ class ScriptDependenciesScannerProcessor {
             CurateOutcomeBuildItem curateOutcome,
             WebBundlerConfig config)
             throws IOException {
-        List<Path> webDeps = curateOutcome.getApplicationModel().getRuntimeDependencies().stream()
+        List<Path> webDeps = StreamSupport
+                .stream(curateOutcome.getApplicationModel().getDependencies(DependencyFlags.REMOVED).spliterator(), true)
                 .filter(Dependency::isJar)
                 .filter(config.dependencies().type()::matches)
                 .map(ResolvedDependency::getResolvedPaths)
