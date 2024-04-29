fb830729060a1e4b628fad743c19dceb2a17f84c
Andy Damevin <ia3andy@gmail.com>
2024-04-29 19:48:44 +0200

    Introduce basic dev-ui (#196)

--- common-deployment/src/main/java/io/quarkiverse/web/bundler/deployment/items/BundleWebAsset.java
+++ common-deployment/src/main/java/io/quarkiverse/web/bundler/deployment/items/BundleWebAsset.java
@@ -7,9 +7,20 @@ import java.util.Optional;
 public record BundleWebAsset(WebAsset webAsset, BundleType type) implements WebAsset {
 
     public enum BundleType {
-        ENTRYPOINT, // index.js, index.ts, index.jsx, index.tsx
-        MANUAL, // Add this to the working directory but do not bundle it (the entrypoint may import it)
-        AUTO // Add this to the working directory and index it automatically as part of the bundle
+        ENTRYPOINT("entry-point"), // index.js, index.ts, index.jsx, index.tsx
+        MANUAL("available for import"), // Add this to the working directory but do not bundle it (the entrypoint may import it)
+        AUTO("auto-imported") // Add this to the working directory and index it automatically as part of the bundle
+        ;
+
+        private String label;
+
+        BundleType(String label) {
+            this.label = label;
+        }
+
+        public String label() {
+            return label;
+        }
     }
 
     @Override
