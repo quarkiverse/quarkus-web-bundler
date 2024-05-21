db928432d5bb5f895f9a6496c84ed5651087b779
Andy Damevin <ia3andy@gmail.com>
2024-05-21 22:17:38 +0200

    Fix NPEs when there is no bundling dir

--- deployment/src/main/java/io/quarkiverse/web/bundler/deployment/web/GeneratedWebResourcesProcessor.java
+++ deployment/src/main/java/io/quarkiverse/web/bundler/deployment/web/GeneratedWebResourcesProcessor.java
@@ -16,6 +16,7 @@ import org.jboss.logging.Logger;
 
 import io.quarkiverse.web.bundler.deployment.WebBundlerConfig;
 import io.quarkiverse.web.bundler.deployment.items.ReadyForBundlingBuildItem;
+import io.quarkiverse.web.bundler.deployment.items.WebBundlerTargetDirBuildItem;
 import io.quarkiverse.web.bundler.deployment.util.PathUtils;
 import io.quarkiverse.web.bundler.runtime.WebBundlerResourceRecorder;
 import io.quarkus.deployment.IsDevelopment;
@@ -40,7 +41,7 @@ public class GeneratedWebResourcesProcessor {
     public void processStaticFiles(
             BuildProducer<NotFoundPageDisplayableEndpointBuildItem> notFoundPageProducer,
             List<GeneratedWebResourceBuildItem> staticResources,
-            ReadyForBundlingBuildItem readyForBundling,
+            WebBundlerTargetDirBuildItem targetDir,
             BuildProducer<GeneratedResourceBuildItem> prodResourcesProducer,
             BuildProducer<NativeImageResourceBuildItem> nativeImageResourcesProducer,
             LiveReloadBuildItem liveReload,
@@ -49,7 +50,7 @@ public class GeneratedWebResourcesProcessor {
             return;
         }
         if (launchModeBuildItem.getLaunchMode().isDevOrTest()) {
-            final Path distDir = readyForBundling.distDir();
+            final Path distDir = targetDir.dist();
             final GeneratedWebResourcesDevContext generatedWebResourcesDevContext = liveReload
                     .getContextObject(GeneratedWebResourcesDevContext.class);
             Map<String, String> hashes = new HashMap<>();
@@ -105,13 +106,13 @@ public class GeneratedWebResourcesProcessor {
     @Record(RUNTIME_INIT)
     public void runtimeInit(
             LaunchModeBuildItem launchMode,
-            ReadyForBundlingBuildItem readyForBundling,
+            WebBundlerTargetDirBuildItem targetDir,
             List<GeneratedWebResourceBuildItem> staticResources,
             WebBundlerResourceRecorder recorder,
             BuildProducer<RouteBuildItem> routes) throws IOException {
         if (!staticResources.isEmpty()) {
             String webDir = launchMode.getLaunchMode().isDevOrTest()
-                    ? readyForBundling.distDir().toAbsolutePath().toString()
+                    ? targetDir.dist().toAbsolutePath().toString()
                     : null;
 
             routes.produce(RouteBuildItem.builder().orderedRoute("/*", DEFAULT_ROUTE_ORDER)
@@ -128,14 +129,15 @@ public class GeneratedWebResourcesProcessor {
     @Record(RUNTIME_INIT)
     public void initChangeEventHandler(
             WebBundlerConfig config,
-            ReadyForBundlingBuildItem readyForBundling,
+            WebBundlerTargetDirBuildItem targetDir,
             WebBundlerResourceRecorder recorder,
+            ReadyForBundlingBuildItem readyForBundling,
             List<GeneratedWebResourceBuildItem> staticResources,
             ShutdownContextBuildItem shutdownContext,
             BuildProducer<RouteBuildItem> routes) {
-        if (config.browserLiveReload()) {
+        if (config.browserLiveReload() && readyForBundling != null) {
             routes.produce(RouteBuildItem.builder().route(WEB_BUNDLER_LIVE_RELOAD_PATH)
-                    .handler(recorder.createChangeEventHandler(readyForBundling.distDir().toAbsolutePath().toString(),
+                    .handler(recorder.createChangeEventHandler(targetDir.dist().toAbsolutePath().toString(),
                             staticResources.stream().map(GeneratedWebResourceBuildItem::publicPath)
                                     .collect(Collectors.toSet()),
                             shutdownContext))
