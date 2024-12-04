1bcb6d8050fa7925100aec13c6c530c49697b0b1
Andy Damevin <ia3andy@gmail.com>
2024-12-04 16:28:22 +0100

    Now works out of the box

---
+++ deployment/src/test/java/io/quarkiverse/web/bundler/test/WebBundlerEmptyTest.java
@@ -0,0 +1,39 @@
+package io.quarkiverse.web.bundler.test;
+
+import jakarta.inject.Inject;
+
+import org.hamcrest.Matchers;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkiverse.web.bundler.runtime.Bundle;
+import io.quarkus.test.QuarkusUnitTest;
+import io.restassured.RestAssured;
+
+public class WebBundlerEmptyTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
+            .overrideConfigKey("quarkus.web-bundler.dependencies.compile-only", "false")
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsResource("web/index.html"));
+
+    @Inject
+    Bundle bundle;
+
+    @Test
+    public void test() {
+        RestAssured.given()
+                .get("/")
+                .then()
+                .statusCode(200)
+                .body(Matchers.containsString(" <script type=\"module\" src=\"" + bundle.script("main") + "\"></script>"));
+        RestAssured.given()
+                .basePath("")
+                .get(bundle.script("main"))
+                .then()
+                .statusCode(200);
+    }
+}
